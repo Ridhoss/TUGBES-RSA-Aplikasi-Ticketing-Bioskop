98e004aec1f0c352f790bc4d3a5ee56ad44cb51c
SalmaArifahZahra git config --global user.email <zsalmaarifah@gmail.com>
2025-06-13 11:58:05 +0700

    Update : Pemisahan HalamanMenuAdmin

--- main.c
+++ main.c
@@ -12,75 +12,7 @@ int main() {
     int loggedIn = 0;  // status login, 0 = belum login
     int idLogin = 1; // 1 = sudah login
     HalamanAwal(&loggedIn, &idLogin);
-    
-    char* rootStr = (char*) malloc(strlen("Head") + 1);
-    strcpy(rootStr, "Head");
-    address root = CreateTree((infotype)rootStr, ROOT);
-
-    if (root == NULL) {
-        printf("Gagal membuat tree.\n");
-        return 1;
-    }
-
-    // Load data dari database
-    LoadKota(root);
-    LoadBioskop(root);
-    LoadTeater(root);
-
-    char parentName[100], childName[100];
-    char pilihan;
-
-    do {
-        printf("\nMenu:\n");
-        printf("1. Manipulasi Kota\n");
-        printf("2. Manipulasi Bioskop\n");
-        printf("3. Manipulasi Film\n");
-        printf("4. -\n");
-        printf("5. -\n");
-        printf("6. Keluar\n");
-        printf("Pilih opsi (1-6): ");
-        scanf(" %c", &pilihan);
-
-        switch (pilihan) {
-            case '1': {
-                HalamanManipulasiKota(root);
-
-                break;
-            }
-            case '2': {
-                HalamanManipulasiBioskop(root);
-
-                break;
-            }
-            case '3': {
-
-                break;
-            }
-            case '4': {
-
-                break;
-            }
-            case '5': {
-
-                break;
-            }
-            case '6': {
-                printf("Keluar dari program.\n");
-                
-                break;
-            }
-            default:
-                printf("Pilihan tidak valid.\n");
-
-                break;
-        }
-    
-    } while (pilihan != '6');
-    
-    if (root != NULL) {
-        DeleteAll(root);
-        root = NULL;
-    }
+    HalamanMenuAdmin();
     
     return 0;
 }
--- views/views.c
+++ views/views.c
@@ -1,7 +1,77 @@
 #include "header/views.h"
 
-// void HalamanMenuAdmin(){
-// }
+void HalamanMenuAdmin(){
+
+    char* rootStr = (char*) malloc(strlen("Head") + 1);
+    strcpy(rootStr, "Head");
+    address root = CreateTree((infotype)rootStr, ROOT);
+
+    if (root == NULL) {
+        printf("Gagal membuat tree.\n");
+        return;
+    }
+
+    LoadKota(root);
+    LoadBioskop(root);
+    LoadTeater(root);
+
+    char parentName[100], childName[100];
+    char pilihan;
+
+    
+    do {
+        printf("\nMenu:\n");
+        printf("1. Manipulasi Kota\n");
+        printf("2. Manipulasi Bioskop\n");
+        printf("3. Manipulasi Film\n");
+        printf("4. -\n");
+        printf("5. -\n");
+        printf("6. Keluar\n");
+        printf("Pilih opsi (1-6): ");
+        scanf(" %c", &pilihan);
+
+        switch (pilihan) {
+            case '1': {
+                HalamanManipulasiKota(root);
+
+                break;
+            }
+            case '2': {
+                HalamanManipulasiBioskop(root);
+
+                break;
+            }
+            case '3': {
+
+                break;
+            }
+            case '4': {
+
+                break;
+            }
+            case '5': {
+
+                break;
+            }
+            case '6': {
+                printf("Keluar dari program.\n");
+                
+                break;
+            }
+            default:
+                printf("Pilihan tidak valid.\n");
+
+                break;
+        }
+    
+    } while (pilihan != '6');
+    
+    if (root != NULL) {
+        DeleteAll(root);
+        root = NULL;
+    }
+
+}
 
 void HalamanManipulasiKota(address root) {
     char namaKota[100];
