09334a7a509f618fc8734405205f631fb96110f5
SalmaArifahZahra git config --global user.email <zsalmaarifah@gmail.com>
2025-06-15 01:42:26 +0700

    Tambah struct di ADTStack

---
+++ models/ADTStack.c
@@ -0,0 +1,7 @@
+#include "header/ADTStack.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+// void CreateStack(StackMenu *S);
+// bool IsEmptyStack(StackMenu S);
---
+++ models/header/ADTStack.h
@@ -0,0 +1,17 @@
+#ifndef ADTStack_h
+#define ADTStack_h
+
+#include "../../library/boolean.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "ADTList.h"
+
+typedef List StackMenu;
+
+// void CreateStack(StackMenu *S);
+// bool IsEmptyStack(StackMenu S);
+
+#endif
