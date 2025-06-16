d833d60b7fd1ac9450adb483895ef0a700489a40
SalmaArifahZahra git config --global user.email <zsalmaarifah@gmail.com>
2025-06-16 23:20:49 +0700

    perbaikan comment

--- models/ADTStack.c
+++ models/ADTStack.c
@@ -1,26 +1,26 @@
 #include "header/ADTStack.h"
 
-// Deskripsi: Membuat stack kosong dengan menginisialisasi list
+// Deskripsi: Prosedur untuk membuat stack kosong dengan menginisialisasi list
 // I.S.: Stack belum terdefinisi atau belum diinisialisasi
 // F.S.: Stack S terinisialisasi sebagai list kosong
 void CreateStack(Stack *S) {
     CreateList(S);
 }
-// Deskripsi: Mengecek apakah stack kosong
+// Deskripsi: Fungsi untuk mengecek apakah stack kosong
 // I.S.: Stack S terdefinisi
 // F.S.: Mengembalikan true jika stack kosong, false jika tidak
 boolean IsEmptyStack(Stack S) {
     return ListEmpty(S);
 }
 
-// Deskripsi:Menambahkan data sebagai elemen stack S (ditambahkan ke paling depan)
+// Deskripsi: Prosedur untuk menambahkan data sebagai elemen stack S (ditambahkan ke paling depan)
 // IS : S mungkin kosong
 // FS : data menjadi elemen teratas (First) yang baru 
 void Push(Stack* S, infotype data) {
     InsFirst(S, data);
 }
 
-// Deskripsi:Menghapus elemen teratas dari Stack S dan mengembalikannya 
+// Deskripsi: Fungsi untuk menghapus elemen teratas dari Stack S dan mengembalikannya 
 // IS : S tidak kosong 
 // FS : Elemen TOP dihapus dari Stack dan nilainya dikembalikan 
 void* Pop(Stack* S) {
@@ -29,7 +29,7 @@ void* Pop(Stack* S) {
     return temp;
 }
 
-// Deskripsi:Mengambil elemen teratas dari Stack S tanpa menghapusnya
+// Deskripsi: Fungsi untuk mengambil elemen teratas dari Stack S tanpa menghapusnya
 // IS : S terdefinisi (mungkin kosong) 
 // FS : Mengembalikan nilai elemen TOP jika ada, NULL jika Stack kosong
 void* Top(Stack S) {
